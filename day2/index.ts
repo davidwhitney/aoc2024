a3eaf64bde67eeea7b6eb7b3f70344c58fc04d65
David Whitney <david@davidwhitney.co.uk>
2024-12-04 07:24:27 -0300

    clean

--- day2/index.ts
+++ day2/index.ts
@@ -16,22 +16,22 @@ export default function day2(input: string, dampen: boolean = false) {
 function isSafeSequence(seq: number[], dampen: boolean) {
     let previousDirection = "";
 
-    for (let i = 1; i < seq.length; i++) {
-        const prev = seq[i - 1];
+    for (let i = 0; i < seq.length - 1; i++) {
         const current = seq[i];
+        const next = seq[i + 1];
 
-        const difference = Math.abs(prev - current);
-        const increasing = prev < current;
-        const decreasing = prev > current;
+        const difference = Math.abs(current - next);
+        const increasing = next > current;
+        const decreasing = next < current;
+        
+        let safe = difference >= 1 && difference <= 3;
 
         if ((increasing && previousDirection === "decreasing")
             || (decreasing && previousDirection === "increasing")) {
-            return false;
+            safe = false;
         }
 
         previousDirection = increasing ? "increasing" : "decreasing";
-
-        const safe = difference >= 1 && difference <= 3;
 
         if (!safe) {
             return false;
