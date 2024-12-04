540375dc6971d9fd0c7d51cd8b10c611e5cce6cb
David Whitney <david@davidwhitney.co.uk>
2024-12-04 08:29:23 -0300

    3-2

--- day3/index.test.ts
+++ day3/index.test.ts
@@ -3,6 +3,7 @@ import { describe, it, assert } from 'vitest';
 import day3 from './index';
 
 const testInput = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`.trim();
+const testInput2 = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))".trim();
 
 const fileContents = fs.readFileSync('./day3/input.txt', 'utf8')
 const input = fileContents.trim();
@@ -16,5 +17,15 @@ describe('Day 2', () => {
     it("part 1 - input", () => {
         const result = day3(input);
         assert.equal(result, 181345830);
+    });
+
+    it("part 2 - do and don't example", () => {
+        const result = day3(testInput2, true);
+        assert.equal(result, 48);
+    });
+
+    it("part 2 - do and don't input", () => {
+        const result = day3(input, true);
+        assert.equal(result, 98729041);
     });
 });
--- day3/index.ts
+++ day3/index.ts
@@ -1,6 +1,10 @@
 const mulRegex = /mul\((\d+),(\d+)\)/g;
 
-export default function(block: string) {
+export default function(block: string, supportDoAndDont = false) {
+    if (supportDoAndDont) {      
+        block = preProcessBlock(block);;
+    }
+
     const occurrences = (block.match(mulRegex) || []);
     return occurrences.reduce((acc, occurrence) => {
         return acc + processOp(occurrence);
@@ -22,4 +26,27 @@ function processOp(op: string) {
     }
 
     return 0;    
+}
+
+function preProcessBlock(block: string) {
+    const blocks = block.split(/(don't)|(do)/g).filter(b => b && b.length > 0);
+
+    let process = true;
+    let newBlock = "";
+
+    for (const b of blocks) {
+        if (b === "do") {
+            process = true;
+            continue;
+        } else if (b === "don't") {
+            process = false;
+            continue;
+        }
+        
+        if (process) {
+            newBlock += b;
+        }
+    }
+
+    return newBlock;
 }
