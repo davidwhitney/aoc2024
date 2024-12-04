f1fffcb96789729837652094c35e6e68f0f4a0b3
David Whitney <david@davidwhitney.co.uk>
2024-12-04 08:52:54 -0300

    4-1

--- day4/index.test.ts
+++ day4/index.test.ts
@@ -2,11 +2,31 @@ import fs from 'fs';
 import { describe, it, assert } from 'vitest';
 import day4 from './index';
 
-const testInput = ``.trim();
+const testInput = `
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`.trim();
 
-const fileContents = fs.readFileSync('./day3/input.txt', 'utf8')
+const fileContents = fs.readFileSync('./day4/input.txt', 'utf8')
 const input = fileContents.trim();
 
 describe('Day 4', () => {
+    it("part 1 - example", () => {
+        const result = day4(testInput);
+        assert.equal(result, 18);
+    });
+
+    it("part 1 - examinputple", () => {
+        const result = day4(input);
+        assert.equal(result, 2567);
+    });
    
 });
+
--- day4/index.ts
+++ day4/index.ts
@@ -1,6 +1,51 @@
-const mulRegex = /mul\((\d+),(\d+)\)/g;
+export default function(input: string) {
+    const grid = input.split('\n').map(row => row.split(''));
+    const width = grid.length;
 
-export default function(block: string, supportDoAndDont = false) {
+    let totalFound = 0;
+    
+    for (let x = 0; x < width; x++) {
+        for (let y = 0; y < width; y++) {
+            const wordsFoundFromLocation = checkGridLocation(grid, x, y);
+            totalFound += wordsFoundFromLocation;            
+        }
+    }
    
-    return 0;
+    return totalFound;
+}
+
+function checkGridLocation(grid: string[][], x: number, y: number) {
+    const startValue = grid[x][y];
+    if (startValue !== 'X') {
+        return 0;
+    }
+
+    let matches = 0;
+
+    const directions = [
+        [1, 0], 
+        [-1, 0], 
+        [0, 1], 
+        [0, -1],
+        [1, 1],
+        [-1, -1],
+        [1, -1],
+        [-1, 1]
+    ];
+
+    for (const direction of directions) {
+        const [dx, dy] = direction;
+        const nextThreeCells = [
+            grid[x + dx]?.[y + dy],
+            grid[x + (2 * dx)]?.[y + (2 * dy)],
+            grid[x + (3 * dx)]?.[y + (3 * dy)]
+        ];
+
+        const value = "X" + nextThreeCells.join(''); //?
+        if (value === 'XMAS') {
+            matches++; //?
+        }
+    }
+
+    return matches;
 }
